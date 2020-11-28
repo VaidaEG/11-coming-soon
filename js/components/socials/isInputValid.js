cf11f358a249970890749c9480491c20d81da9d2
VaidaEG <grausliene.vaida@gmail.com>
2020-11-28 16:07:15 +0200

    Code documentation

--- js/components/socials/isInputValid.js
+++ js/components/socials/isInputValid.js
@@ -1,3 +1,9 @@
+/**
+ * renderSocial() funkcijai skirtu ivesties duomenu (input params) validacija.
+ * @param {string} selector Selektorius, kaip rasti norima vieta, kur bus istatomas sugeneruotas kodas
+ * @param {Array} data  Duomenu masyvas su objektais, kurie reprezentuoja social nuorodas
+ * @returns {boolean} Jei patikrinimo metu randama logine duomenu klaida, grazinama `false`, priesingu atveju - `true`
+ */
 function isInputValid(selector, data) {
     if (typeof selector !== 'string') {
         console.error('ERROR: selektorius turi būti tekstinio tipo.')
